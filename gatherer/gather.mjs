a7a6f2471ec709ee2eca1faf4094b78bdc4bca56
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2021-03-29 22:12:25 +0200

    Added Netto shops.

--- gatherer/gather.mjs
+++ gatherer/gather.mjs
@@ -2,3 +2,5 @@ import './biedronka/gather.mjs';
 import './inpost/gather.mjs';
 import './lidl/gather.mjs';
 import './mpk/gather.mjs';
+import './netto/gather.mjs';
+import './pharmacy/gather.mjs';
--- gatherer/parse.mjs
+++ gatherer/parse.mjs
@@ -2,3 +2,5 @@ import './biedronka/parse.mjs';
 import './inpost/parse.mjs';
 import './lidl/parse.mjs';
 import './mpk/parse.mjs';
+import './netto/parse.mjs';
+import './pharmacy/parse.mjs';
